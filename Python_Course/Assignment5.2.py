fce5e5b090f111f894cbc15ea422a6c6a8ba2c5d
KshitijSangar217 <58737378+KshitijSangar217@users.noreply.github.com>
2020-09-30 21:03:37 +0530

    Added an example in the file

--- Python_Course/Assignment5.2.py
+++ Python_Course/Assignment5.2.py
@@ -19,3 +19,17 @@ while True:
 
 print("Maximum is", largest)
 print("Minimum is", smallest)
+
+#Another way of putting this problem statement in an efficient way.
+def largest(a, b):
+    if a > b:
+        return a
+    else if b > a:
+        return b
+    else:
+        return "Both"
+largest_No = str(largest(5,6))
+if(largest_No == "Both"):
+    print("Both the numbers are equal")
+else:
+    print("Largest number is : ", largest_No)
